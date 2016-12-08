3c1f1e8374923df705ba61807fa867f7d5cd85c2
cobbb1 <531138675@qq.com>
2016-12-08 18:03:31 +0800

    1

--- htm/geq.js
+++ htm/geq.js
@@ -1,16 +1,31 @@
-function gz(x,y,p,wa,wb,wc){
+function gz(x,y,p,wa,wb,wc,b){
 	e = [0,0];
 	for(o=0;o<p.length;o++){
-	   w = wa*p[o][2]+wb*p[o][3]+wc*p[o][4];
+	   w = wa*p[o][3]/p[o][2]+wb*p[o][4];
 	   z = (p[o][0]-x)*(p[o][0]-x)+(p[o][1]-y)*(p[o][1]-y);
 	   z = Math.sqrt(z);
-	   if(z<0.01) z = 0.01;
+	   if(z<1) z = 1;
 
 	   wx = (p[o][0]-x)/z;
 	   wy = (p[o][1]-y)/z;
+
+	   z = z*z;
 	   e[0] = e[0] - wx*w/(z*z*z);
 	   e[1] = e[1] - wy*w/(z*z*z);
 	}
 
-	return e
+	for(m=0;m<b.length;m++){
+	   w = wc*b[m][2]
+	   z = (b[m][0]-x)*(b[m][0]-x)+(b[m][1]-y)*(b[m][1]-y);
+	   z = Math.sqrt(z);
+	   if(z<1) z = 1;
+
+	   wx = (b[m][0]-x)/z;
+	   wy = (b[m][1]-y)/z;
+	   z = z*z;
+	   e[0] = e[0] - wx*w/(z*z*z);
+	   e[1] = e[1] - wy*w/(z*z*z);
+	}
+
+	return e;
 }
